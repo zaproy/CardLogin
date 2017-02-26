6e521cf2d667c39a9945bd0fdc7e3918c837423f
Zaproy <misty.social@gmail.com>
2017-02-27 00:23:54 +0700

    Add project to git

---
+++ Assets/LogIn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogIn : MonoBehaviour {
+
+	public InputField user, pass;
+	public string username,password;
+	public GameObject wrongTx;
+
+	void Start () {
+		
+		wrongTx.SetActive (false);
+	}
+
+	public void LoginSys(){
+		if (user.text == username && pass.text == password) {
+			SceneManager.LoadScene (1);
+		} else {
+			wrongTx.SetActive (true);
+		}
+	}
+}
---
+++ Assets/UserData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UserData : MonoBehaviour {
+
+	public Text cardNum;
+	public Text userName;
+	public Text point;
+	public Text[] menu;
+
+	// Use this for initialization
+	void Start () {
+		cardNum.text = "180012568700";
+		userName.text = "Saranya Khanjanalekha";
+		point.text = "150";
+		menu[0].text = "Capucino";
+		menu[1].text = "Mocha";
+		menu[2].text = "Chocolate";
+		menu[3].text = "Matcha";
+	}
+	
+	public void LogOut(){
+		SceneManager.LoadScene (0);
+	}
+}
